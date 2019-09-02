1af5dc4377e3977f7178014c8198d431151c76da
crabtree <justin.kyle.crabtree@gmail.com>
2019-09-01 20:11:38 -0400

    adds query/url/body param routes to actor controller

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,9 +1,19 @@
 class Api::ActorsController < ApplicationController
 
-  def actor_action
-    @actor = Actor.find_by(id: 5)
+  def show
+    @actor = Actor.find_by(id: params[:id])
     render 'actor.json.jb'
   end
 
+  def show_wildcard
+    id = params[:id]
+    @actor = Actor.find_by(id: id)
+    render 'actor.json.jb'
+  end
+
+  def body
+    @actor = Actor.find_by(id: params[:id])
+    render 'actor.json.jb'
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,16 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/actor" => "actors#actor_action"
+    get "/actor" => "actors#show"
+
+    get "/actor/:id" => "actors#show_wildcard"
+
+    post "/actor" => "actors#body"
 
     get "/movies_all_app" => "movies#movie_all_action"
 
     get "/movie_single_app" => "movies#movie_single_action"
+
   end
 end
 
