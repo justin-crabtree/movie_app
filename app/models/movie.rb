fff13652abbe39e6a33831bce81d758f90bdbae7
crabtree <justin.kyle.crabtree@gmail.com>
2019-09-15 15:00:41 -0400

    adds genre and moviegenre models and associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,5 +12,11 @@ class Movie < ApplicationRecord
   # end
 
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
+  # def genre_names
+  #   genres.map do { |genre| genre.name }
+  # end
 
 end
