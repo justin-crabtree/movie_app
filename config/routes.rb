e18ccea2d6b73aa09ac9869cfed032f3fdbc7f7d
crabtree <justin.kyle.crabtree@gmail.com>
2019-09-16 17:36:21 -0400

    adds movie_genre controller and create action

--- config/routes.rb
+++ config/routes.rb
@@ -5,28 +5,21 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/actors/:id" => "actors#show"
-
     get "/actors" => "actors#index"
-
     post "/actors" => "actors#create"
-
     patch "/actors/:id" => "actors#update"
-
     delete "/actors/:id" => "actors#destroy"
 
     get "/movies" => "movies#index"
-
     get "/movies/:id" => "movies#show"
-
     post "/movies" => "movies#create"
-
     patch "/movies/:id" => "movies#update"
-
     delete "/movies/:id" => "movies#destroy"
 
     post "/users" => "users#create"
-
     post "/sessions" => "sessions#create"
+
+    post "/movie_genres" => "movie_genres#create"
 
   end
 end
